5566a40b4d233f0fee609ed027370d9f5e657e00
sarcasticccccc <abhisheksingh.cs19@gmail.com>
2026-01-02 17:03:50 +0530

    changes

--- Snake Game/scripts.js
+++ Snake Game/scripts.js
@@ -1,21 +1,28 @@
 const board = document.querySelector('.board')
-const startGameModal=   document.querySelector('.start-game')
-const gameOverModal=   document.querySelector('.game-over')
+const startGameModal = document.querySelector('.start-game')
+const gameOverModal = document.querySelector('.game-over')
 const startButton = document.querySelector('.btn-start')
+const restartButton = document.querySelector('.btn-restart')
 const Modal = document.querySelector('.modal')
-const cols = Math.floor(board.clientWidth / blockWidth)
-const rows = Math.floor(board.clientHeight / blockHeight)
 let timer = null
 const blockWidth = 50
 const blockHeight = 50
+const cols = Math.floor(board.clientWidth / blockWidth)
+const rows = Math.floor(board.clientHeight / blockHeight)
+const highScoreElement = document.querySelector('.high-score')
+const scoreElement = document.querySelector('.score')
+const timeElement = document.querySelector('.time')
 
+let highScore = 0
+let score = 0
+let time = 0
 
 let food = {
     x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)
 }
 
 const blocks = []
-const snake = [
+let snake = [
     { x: 1, y: 3 },
 ]
 
@@ -58,22 +65,38 @@ function render() {
     else if (direction === 'down') {
         head = { x: snake[0].x + 1, y: snake[0].y }
     }
+
+
+    // Game Over Logic is there
     
     if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
         // alert("Game Over")
-        Modal.style.display='flex'
-startGameModal.style.display='none'
-        gameOverModal.style.display='flex'
+        Modal.style.display = 'flex'
+        startGameModal.style.display = 'none'
+        gameOverModal.style.display = 'flex'
+        if(highScore<score){
+            highScore=score
+        }
+        
+
+        highScoreElement.textContent=highScore
+        score=0
+        scoreElement.textContent=score
         clearInterval(timer)
     }
 
     if (head.x == food.x && head.y == food.y) {
         blocks[`${food.x}-${food.y}`].classList.remove('food')
-        food={
-            x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)
+        food = {
+            x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)
         }
         blocks[`${food.x}-${food.y}`].classList.add('food')
-snake.unshift(head)
+        snake.unshift(head)
+        score+=10
+        scoreElement.textContent=score
+        
+        
+        
     }
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
@@ -90,15 +113,29 @@ snake.unshift(head)
 //     render()
 // }, 500)
 
-startButton.addEventListener('click',()=>{
-    timer=setInterval(()=>{
-        Modal.style.display='none'
+
+startButton.addEventListener('click', () => {
+    timer = setInterval(() => {
+        console.log('its working');
+
+        Modal.style.display = 'none'
         render()
-    },300)
+    }, 100)
 })
 
-function restartGame(){
-
+restartButton.addEventListener('click', restartGame)
+function restartGame() {
+    blocks[`${food.x}-${food.y}`].classList.remove('food')
+    // snake.forEach(segment => {
+    //     blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+    // })
+    direction = 'down'
+    Modal.style.display = 'none'
+    snake = [{ x: 1, y: 3 }]
+    food = {
+        x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)
+    }
+    timer = setInterval(() => { render() }, 100)
 }
 
 addEventListener('keydown', (event) => {
