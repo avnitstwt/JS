d48517abe8a72c14ce913a79082c024f71d3708b
sarcasticccccc <abhisheksingh.cs19@gmail.com>
2026-01-04 18:21:08 +0530

    cahnges

--- KanbanProject/script.js
+++ KanbanProject/script.js
@@ -1,1 +1,14 @@
+    const todo =document.querySelector('.TODO')
+    const progress=document.querySelector('.PROGRESS')
+    const completed =document.querySelector('.COMPLETED')
+    const tasks = document.querySelectorAll('.tasks')
+
+    tasks.forEach((task)=>{
+        task.addEventListener('drag',(e)=>{
+            console.log(('dragging',e));
+            
+        })
+    })
     
+    console.log(todo);
+
